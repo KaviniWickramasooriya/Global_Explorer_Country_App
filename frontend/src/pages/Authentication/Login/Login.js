794a8265792a8d47247684847b559f93316a238c
Kavini Wickramasooriya <165365634+KaviniWickramasooriya@users.noreply.github.com>
2025-05-04 00:41:43 +0530

    kv

--- frontend/src/pages/Authentication/Login/Login.js
+++ frontend/src/pages/Authentication/Login/Login.js
@@ -35,7 +35,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('global-explorer-country-app.vercel.app/login', formData);
+      const response = await axios.post('/login', formData);
       const { token } = response.data;
 
       // Store token in localStorage
